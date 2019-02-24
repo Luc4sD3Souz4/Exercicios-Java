0a4904f5a83ff01ac3d0818fdc87c0e08a90c69d
Lucas De Souza <37718781+Luc4sD3Souz4@users.noreply.github.com>
2019-02-24 10:54:26 -0300

    Add files via upload

---
+++ Retangulo/Retangulo.java
@@ -0,0 +1,29 @@
+
+public class Retangulo {
+	
+	private Float comprimento = 1.0f;
+	private Float largura 	  = 1.0f;
+	
+	Valida val = new Valida();
+	
+	public Float getLargura() {
+		return largura;
+	}
+	public void setLargura( Float largura ) {
+		val.validador( largura );
+		this.largura = largura;
+	}
+	
+	public Float getComprimento() {
+		return comprimento;
+	}
+	public void setComprimento( Float comprimento ) {
+		val.validador( comprimento );
+		this.comprimento = comprimento;
+	}	
+	
+	public void Calculo() {
+		System.out.println( " Área: " + getLargura() * getComprimento() );
+		System.out.println( " Perí­metro: " + ( getLargura() + getLargura() ) + ( getComprimento() + getComprimento() )   );
+	}
+}
---
+++ Retangulo/Valida.java
@@ -0,0 +1,9 @@
+
+public class Valida { 
+	public void validador( Float largura ) {
+		if( largura <= 0.0 || largura <= 0  ) {
+			System.out.println( " Erro: Um dos lados do retângulo é igual ou menor que zero " );
+			System.exit(0);
+		}
+	}
+}
